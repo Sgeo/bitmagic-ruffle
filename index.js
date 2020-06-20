5ccbfe938f7a0e9724ed1747e026bce51f6dea57
Sgeo <sgeoster@gmail.com>
2020-06-20 04:14:18 -0400

    * Play BM files * Attempt to block window.location navigation (currently not working)

--- index.js
+++ index.js
@@ -27,21 +27,60 @@
         }
     }
 
+    let realWindow = window;
+    window = new Proxy(window, {
+        set: function(obj, prop, newval) {
+            console.log("Set on window");
+            if(prop === "location" && newval?.startsWith?.("/bmcontrol/")) {
+                bmcontrolHandler(newval);
+            } else {
+                realWindow[prop] = newval;
+            }
+        }
+    });
+
     const FILE_SELECTOR = document.querySelector("#file-selector");
     const MAIN_ELEMENT = document.querySelector("#main");
 
-    FILE_SELECTOR.addEventListener("change", function(e) {
+    
+
+    async function bm_to_swf(bm_file) {
+        const DECODER = new TextDecoder("windows-1252");
+        
+        let buffer = await new Promise((resolve, reject) => {
+            let reader = new FileReader();
+            reader.onload = () => {
+                resolve(reader.result);
+            };
+            reader.onerror = (err) => {
+                reject(err);
+            };
+            reader.readAsArrayBuffer(bm_file);
+        });
+
+        let view = new DataView(buffer);
+        let header_size = view.getUint32(0x04, false);
+
+        let ext = DECODER.decode(buffer.slice(0x1C, 0x1F));
+        if(ext !== "swf") {
+            if(ext === "dcr") alert("Sorry, Director BitGames are not supported.");
+            else alert(`Sorry, unrecognized extension .${ext}.`);
+            throw "Unrecognized format";
+        }
+
+        return buffer.slice(header_size);
+
+
+    }
+
+    FILE_SELECTOR.addEventListener("change", async function(e) {
         MAIN_ELEMENT.innerHTML = "";
         let ruffle = RufflePlayer.newest();
         let player = ruffle.create_player();
         MAIN_ELEMENT.appendChild(player);
         let file = this.files[0];
         if(file) {
-            let reader = new FileReader();
-            reader.onload = function() {
-                player.play_swf_data(reader.result);
-            };
-            reader.readAsArrayBuffer(file);
+            player.play_swf_data(await bm_to_swf(file));
         }
     });
 
