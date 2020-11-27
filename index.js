574ac13e803d36fd2aac64a07da7ee8f861fdeac
Sgeo <sgeoster@gmail.com>
2020-11-26 21:06:16 -0500

    Process BitMagic metadata

--- index.js
+++ index.js
@@ -16,6 +16,29 @@
     const FILE_SELECTOR = document.querySelector("#file-selector");
     const MAIN_ELEMENT = document.querySelector("#main");
 
+    function read_bm_meta(decoder, view) {
+        let index = 0x18;
+        function read_bm_string() {
+            let string_size = view.getUint32(index, false);
+            index += 4;
+            let string = decoder.decode(view.buffer.slice(index, index + string_size));
+            index += string_size;
+            return string;
+        }
+        let extension = read_bm_string();
+        let name = read_bm_string();
+        let note = read_bm_string();
+        let edition = view.getUint32(index, false);
+        return {extension, name, note, edition};
+    }
+
+    function show_bm_meta(meta) {
+        document.getElementById("name").textContent = meta.name;
+        document.getElementById("note").textContent = meta.note;
+        document.getElementById("edition").textContent = meta.edition;
+        document.getElementById("extension").textContent = meta.extension;
+    }
+
     
 
     async function bm_to_swf(bm_file) {
@@ -35,7 +58,10 @@
         let view = new DataView(buffer);
         let header_size = view.getUint32(0x04, false);
 
-        let ext = DECODER.decode(buffer.slice(0x1C, 0x1F));
+        let meta = read_bm_meta(DECODER, view);
+        show_bm_meta(meta);
+
+        let ext = meta.extension;
         if(ext !== "swf") {
             if(ext === "dcr") alert("Sorry, Director BitGames are not supported.");
             else alert(`Sorry, unrecognized extension .${ext}.`);
